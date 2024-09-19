86bf9b9f1239747001dd060f5ce7bcb2a91570a6
Michael Reed <mrkr2004@gmail.com>
2024-09-19 18:13:58 -0400

    Fully functioning calculator with no ui yet

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -17,39 +17,72 @@ export default function CalculatorComponent() {
   const [showResults, setShowResults] = React.useState(false);
   const badInputFlag: boolean[] = [false, false, false, false]
   const [showSwitch, setShowSwitch] = React.useState(false);
-
+  const [bornFlag, setBornFlag] = React.useState(false);
   
   const handleSwitchChange = () => {
     setChecked(!checked);
+    console.log(checked.toString());
+    let num:number = 0;
+    displayVals(input, num);
   };
-  
   const handleBornChange = () => {
     input.born.focusTextInput;
     const element = document.getElementById("bornError")!;
-    if(input.born.textInput.current){
+    if(input.born.textInput.current && input.resident.textInput.current){
         const temp = Number(input.born.textInput.current.value);
         if(temp >= 1900 && temp < 2025) {
           element.textContent = null;
           if(temp == 2006){
               setShowSwitch(true);
+              handleSwitchChange();
           } else setShowSwitch(false);
           badInputFlag[0] = false;
+          let num:number = 0;
+          displayVals(input, num);
+          if(badInputFlag[1] || (Math.floor(Number(input.born.textInput.current.value)) > Math.floor(Number(input.resident.textInput.current.value)))) handleResidentChange();
           return;
         }
     }
     badInputFlag[0] = true;
+    for(let i = 0; i < badInputFlag.length; i++) {
+      console.log(badInputFlag[i].toString());
+    }
     element.textContent = "Oops! Please enter a number between 1900-2024";
     }
+
+  const handleResidentChange = () => {
+    input.resident.focusTextInput;
+    const element = document.getElementById("residentError")!;
+    if(input.resident.textInput.current && input.born.textInput.current){
+      const temp = Number(input.resident.textInput.current.value);
+      if(temp >= Number(input.born.textInput.current.value) && temp < 2025) {
+        badInputFlag[1] = false;
+        element.textContent = null;
+        let num:number = 0;
+        displayVals(input, num);
+        return;
+      }
+      if(temp == 0 && bornFlag == false) {
+        setBornFlag(true);
+        element.textContent = null;
+        return;
+      }
+    }
+    element.textContent = "Oops! Your response cannot be a year before you were born or in the future!"
+    badInputFlag[1] = true;
+    let num:number = 0;
+    displayVals(input,num);
+  }
   
   const Switch = () => {
     return (
-                <Form.Check type="switch" 
-                label="I am over 18."
-                id="check18"
-                checked={checked}
-                onChange={handleSwitchChange}
-                >
-                </Form.Check> )
+      <Form.Check type="switch" 
+      label="I am over 18."
+      id="check18"
+      checked={checked}
+      onChange={handleSwitchChange}
+      >
+      </Form.Check> )
 
   }
 
@@ -59,17 +92,65 @@ export default function CalculatorComponent() {
     Your TFSA contribution limit is: 
     </div> );
   }
-  const radio = 0;
-    
+  
+  const handleContributionChange = ()  => {
+    input.contributed.focusTextInput;
+    const element = document.getElementById("contributedError")!;
+    let temparray = input.getTFSAProps(input);
+    temparray[2] = "0";
+    let maxRoom = tfsaMath(temparray);
+    let num: number = 0;
+    if(input.contributed.textInput.current && input.withdrawn.textInput.current) {
+      
+      if(Math.floor(Number(input.contributed.textInput.current.value)) <= maxRoom && Math.floor(Number(input.contributed.textInput.current.value)) >= 0) {
+        badInputFlag[2] = false;
+        element.textContent = null;
+        displayVals(input, num);
+        if(badInputFlag[3] == true || Math.floor(Number(input.withdrawn.textInput.current.value)) > Math.floor(Number(input.contributed.textInput.current.value))) handleWithdrawalChange();
+        return;
+      }
+      if(Math.floor(Number(input.contributed.textInput.current.value)) < 0) {
+        badInputFlag[2] = true;
+        element.textContent = "Oops! negative values for contribution are not allowed! Please enter a positive number";
+        return;
+      }
+    }
+    badInputFlag[2] = true;
+    displayVals(input, num);
+    element.textContent = "Warning: The number you have entered is higher then your maximum contribution limit and withdrawal amount! You will pay interest with this amount";
+  }
+  const handleWithdrawalChange = () => {
+    input.withdrawn.focusTextInput;
+    const element = document.getElementById("withdrawnError")!;
+    const num: number = 0;
+    if(input.withdrawn.textInput.current && input.contributed.textInput.current) {
+      if(Math.floor(Number(input.withdrawn.textInput.current.value)) <= Math.floor(Number(input.contributed.textInput.current.value)) && Math.floor(Number(input.withdrawn.textInput.current.value)) >= 0) {
+        badInputFlag[3] = false;
+        element.textContent = null;
+        displayVals(input, num);
+        handleContributionChange();
+        return;
+      }
+    }
+    badInputFlag[3] = true;
+    element.textContent = "Oops! Value must be a non-negative number less then amount contributed. Please try again!";
+    displayVals(input, num);
+  }
   
   function displayVals(theClass: TFSAvars, contributionRoom: number){
-    
+    const element = document.getElementById("calculation")!;
+    for(let i = 0; i < 4; i++){
+      if(badInputFlag[i] == true || (checked == true && showSwitch == true)) {
+        let temp:Number = tfsaMath(["keyword","","",""]);
+        element.textContent = temp.toString();
+        setShowResults(true);
+        return;
+      }
+    }
     let temp = theClass.getTFSAProps(theClass);
     contributionRoom = tfsaMath(temp);
-    console.log(contributionRoom);
-    console.log(checked.toString());
     setShowResults(true);
-    const element = document.getElementById("calculation")!;
+    
     if(element ==null){
       alert("oops");
     }
@@ -91,7 +172,6 @@ export default function CalculatorComponent() {
             <Form.Control type="number" placeholder="XXXX" ref={input.born.textInput} onBlur={handleBornChange} />
             <Form.Text className="errorMessage" id="bornError" muted> </Form.Text>
           </Form.Group>
-        
           {
             <Form.Group className="mb-3" >
             {showSwitch ? < Switch /> : null }
@@ -133,26 +213,23 @@ export default function CalculatorComponent() {
 
           <Form.Group className="mb-3" controlId="canadianResidentYear">
             <Form.Label>Since what year have you been a Canadian Resident?</Form.Label>
-            <Form.Control type="number" placeholder="XXXX" ref={input.resident.textInput} onChange={input.resident.focusTextInput} />
+            <Form.Control type="number" placeholder="XXXX" ref={input.resident.textInput} onBlur={handleResidentChange} />
+            <Form.Text className="errorMessage" id="residentError"  muted> </Form.Text>
             
             
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="tfsaContributed">
             <Form.Label>How much have you contributed to your TFSA so far?</Form.Label>
-            <Form.Control type="number" ref={input.contributed.textInput} onChange={input.contributed.focusTextInput}/>
+            <Form.Control type="number" ref={input.contributed.textInput} onBlur={handleContributionChange}/>
+            <Form.Text className="errorMessage" id="contributedError" muted></Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="tfsaWithdrawn">
             <Form.Label>How much have you withdrawn from your TFSA so far?</Form.Label>
-            <Form.Control type="number" ref={input.withdrawn.textInput} onChange={input.withdrawn.focusTextInput} />
+            <Form.Control type="number" ref={input.withdrawn.textInput} onBlur={handleWithdrawalChange} />
+            <Form.Text className ="errorMessage" id="withdrawnError" muted></Form.Text>
           </Form.Group>
-
-
-
-          <Button variant="dark" type="button" onClick={() => displayVals(input, contributionRoom)}>
-            Submit
-          </Button>
 
         </Form>
         <div> { showResults ? <Results /> : null } </div>
--- src/components/tfsaMath.tsx
+++ src/components/tfsaMath.tsx
@@ -2,6 +2,7 @@
 
 
 export default function tfsaMath(inputArray: string[]) {
+    if(inputArray[0] == "keyword") return 0;
     let inputs = [Math.floor(Number(inputArray[0])), Math.floor(Number(inputArray[1])), Math.floor(Number(inputArray[2])), Math.floor(Number(inputArray[3]))];
     let yearlyRate = [7000,6500,6000,6000,6000,6000,5500,5500,5500,10000,5500,5500,5000,5000,5000,5000];
     let total = 0;
@@ -22,6 +23,7 @@ export default function tfsaMath(inputArray: string[]) {
     }
 
     total = total - inputs[2] + inputs[3];
+    if (total < 0) total = 0;
     console.log(total);
     return total;
     
