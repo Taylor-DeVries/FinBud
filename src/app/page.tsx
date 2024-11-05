396f8a07901c0b704b855f6f08ce49f9d6b501e3
taylor-devries <taylor.devries@hotmail.com>
2024-11-04 21:17:59 -0500

    removing wave hand emoji

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,44 +7,44 @@ import Textbox from "@/components/Textbox";
 import { useRouter } from "next/navigation";
 
 const HomePage: React.FC = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <div className="h-screen flex items-center justify-center">
-            {/* Parent container for image and text */}
-            <div className="flex flex-col-reverse sm:flex-row items-center ">
-                {/* Image container */}
-                <div className="sm:w-1/3 flex justify-center sm:justify-start sm:mt-64">
-                    <Image
-                        src="/images/Fin.png"
-                        alt="Logo"
-                        width={300}
-                        height={300}
-                        className="w-auto h-auto"
-                        priority
-                        unoptimized
-                    />
-                </div>
-
-                {/* Both text boxes */}
-                <div className="sm:w-2/3">
-                    <Textbox
-                        label="Hi!👋 I’m Fin, your Virtual Finance Buddy."
-                        paddingBetween
-                        secondaryLabel="I’m here to guide you through every stage of your personal finance journey."
-                        chatBubble
-                    />
-
-                    <div className="mt-8 flex justify-center ">
-                        <Button
-                            label="Start My Journey"
-                            onClick={() => router.push("/quiz")}
-                        />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="h-screen flex items-center justify-center">
+      {/* Parent container for image and text */}
+      <div className="flex flex-col-reverse sm:flex-row items-center ">
+        {/* Image container */}
+        <div className="sm:w-1/3 flex justify-center sm:justify-start sm:mt-64">
+          <Image
+            src="/images/Fin.png"
+            alt="Logo"
+            width={300}
+            height={300}
+            className="w-auto h-auto"
+            priority
+            unoptimized
+          />
         </div>
-    );
+
+        {/* Both text boxes */}
+        <div className="sm:w-2/3">
+          <Textbox
+            label="Hi! I’m Fin, your Virtual Finance Buddy."
+            paddingBetween
+            secondaryLabel="I’m here to guide you through every stage of your personal finance journey."
+            chatBubble
+          />
+
+          <div className="mt-8 flex justify-center ">
+            <Button
+              label="Start My Journey"
+              onClick={() => router.push("/quiz")}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
