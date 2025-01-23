8cddbd8587486636032acbfc636c950e71712364
Aiden25451 <a.asprakis@gmail.com>
2025-01-22 22:17:42 -0500

    Working .net8 with aws lambda

--- FinBud-Backend/Program.cs
+++ FinBud-Backend/Program.cs
@@ -1,11 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapGet("/say-hi", () => Results.Ok("I love you"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
