9d5f6b2fa9488190a1026ea7f0822990045cbebd
Aiden25451 <a.asprakis@gmail.com>
2025-03-22 18:51:49 -0400

    show errors

--- apps/frontend/src/app/api/auth/[auth0]/route.js
+++ apps/frontend/src/app/api/auth/[auth0]/route.js
@@ -1,10 +1,10 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+// export const GET = handleAuth();
 
-// export const GET = handleAuth({
-//   onError(req, res, error) {
-//     console.error('Auth0 error:', error); // Log error details
-//     res.status(error.status || 500).end(error.message);
-//   },
-// });
+export const GET = handleAuth({
+  onError(req, res, error) {
+    console.error('Auth0 error:', error); // Log error details
+    res.status(error.status || 500).end(error.message);
+  },
+});
