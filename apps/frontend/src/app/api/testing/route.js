e57720447953cf04cb55cf7b7877b3907adce021
Aiden25451 <a.asprakis@gmail.com>
2025-03-22 21:11:47 -0400

    update routes

--- apps/frontend/src/app/api/testing/route.js
+++ apps/frontend/src/app/api/testing/route.js
@@ -1,7 +1,7 @@
 export async function GET() {
   return new Response(
     JSON.stringify({
-      message: "Debug Check",
+      message: "Debug Check PROOF",
       env: process.env.AUTH0_BASE_URL,
     }),
     {
