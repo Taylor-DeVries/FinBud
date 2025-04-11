9531460c0df1e2337d79b0806ca4189fd5072981
taylor-devries <taylor.devries@hotmail.com>
2025-04-11 15:26:24 -0400

    creating template pages for the more info and adding to sidebar

---
+++ apps/frontend/src/app/info/about/page.tsx
@@ -0,0 +1,8 @@
+export default function aboutUs() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">About Us</h1>
+      <p className="mt-4 text-lg">We are a team of passionate developers.</p>
+    </div>
+  );
+}
---
+++ apps/frontend/src/app/info/disclaimer/page.tsx
@@ -0,0 +1,7 @@
+export default function contactUs() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Disclaimer</h1>
+    </div>
+  );
+}
