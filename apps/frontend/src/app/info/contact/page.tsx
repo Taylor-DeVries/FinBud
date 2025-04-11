a62967f65b92aea49f6cbab5d1fb3b58829edf3e
taylor-devries <taylor.devries@hotmail.com>
2025-04-11 18:15:37 -0400

    small content update

--- apps/frontend/src/app/info/contact/page.tsx
+++ apps/frontend/src/app/info/contact/page.tsx
@@ -35,10 +35,10 @@ export default function contactUs() {
       <p className="mt-2 lg:text-lg md:text-lg sm:text-md text-sm text-gray-50">
         Any questions or concerns:{' '}
         <a
-          href="mailto:finbud-team@gmail.com"
+          href="mailto:finbud.team@gmail.com"
           className="text-link_color hover:text-dark_blue"
         >
-          finbud-team@gmail.com
+          finbud.team@gmail.com
         </a>
       </p>
     </div>
