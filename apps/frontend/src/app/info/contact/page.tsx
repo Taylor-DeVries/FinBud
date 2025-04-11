4a7564bf4838c7219581f461c688a31bc7d009a3
taylor-devries <taylor.devries@hotmail.com>
2025-04-11 17:26:47 -0400

    about us, contact us, and disclaimer pages made

--- apps/frontend/src/app/info/contact/page.tsx
+++ apps/frontend/src/app/info/contact/page.tsx
@@ -1,7 +1,46 @@
 export default function contactUs() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Contact Us</h1>
+    <div className="flex flex-col items-left h-screen my-10 mx-10">
+      <h1 className="lg:text-4xl md:text-4xl text-3xl font-bold text-gray-50">
+        Contact Us
+      </h1>
+      <hr className="border-gray-50 border-4 my-4" />
+      <h2 className="lg:text-2xl md:text-xl sm:text-lg text-lg font-bold text-gray-50">
+        Our Socials
+      </h2>
+      <p className="mt-2 lg:text-lg md:text-lg sm:text-md text-sm text-gray-50">
+        Instagram:{'  '}
+        <a
+          href="https://www.linkedin.com/company/fin-bud/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-link_color underline hover:text-dark_blue"
+        >
+          instagram.com/fin_bud_
+        </a>
+        <br />
+        LinkedIn:{' '}
+        <a
+          href="https://www.linkedin.com/company/fin-bud/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-link_color underline hover:text-dark_blue"
+        >
+          LinkedIn.com/company/fin-bud/
+        </a>
+      </p>
+      <h2 className="mt-4 lg:text-2xl md:text-xl sm:text-lg text-lg font-bold text-gray-50">
+        Our Email
+      </h2>
+      <p className="mt-2 lg:text-lg md:text-lg sm:text-md text-sm text-gray-50">
+        Any questions or concerns:{' '}
+        <a
+          href="mailto:finbud-team@gmail.com"
+          className="text-link_color hover:text-dark_blue"
+        >
+          finbud-team@gmail.com
+        </a>
+      </p>
     </div>
   );
 }
--- apps/frontend/src/app/info/disclaimer/page.tsx
+++ apps/frontend/src/app/info/disclaimer/page.tsx
@@ -1,7 +1,19 @@
 export default function contactUs() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Disclaimer</h1>
+    <div className="flex flex-col items-left h-screen mt-10 ml-10 mr-10">
+      <h1 className="lg:text-4xl md:text-4xl text-3xl font-bold text-gray-50">
+        Disclaimer
+      </h1>
+      <hr className="border-gray-50 border-4 my-4" />
+      <p className="lg:text-lg md:text-lg sm:text-md text-sm text-gray-50">
+        This website is strictly for Canadian users and is for educational and
+        entertainment purposes only. We are not financial advisors, and the
+        content should not be considered professional financial advice. The
+        content is designed to be simple and may not meet everyone's financial
+        needs or goals. Please note that the website is still in development,
+        and content may change. Consult a certified financial professional
+        before making any financial decisions.
+      </p>
     </div>
   );
 }
