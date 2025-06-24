4bc302d20913ec99738107acc67ffc931312cb6c
miche <michelleskater28@gmail.com>
2025-06-23 23:59:19 -0400

    fixed tooltip styling

--- apps/frontend/src/_components/Dashboard-Component/Toolbox/Toolbox.tsx
+++ apps/frontend/src/_components/Dashboard-Component/Toolbox/Toolbox.tsx
@@ -3,29 +3,44 @@ import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
 import { SpeedDialIcon } from '@mui/material';
 import FhsaCalculatorComponent from '@/_components/Calculator-Component/FHSA/FhsaCalculatorComponent';
+// import { Tooltip } from 'react-tooltip';
+import Tooltip from '@mui/material/Tooltip';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 export default function Toolbox() {
   const handleCalculatorClick = () => {
     console.log('Calculator clicked');
   };
 
+  const theme = createTheme({
+    components: {
+      MuiTooltip: {
+        styleOverrides: {
+          tooltip: {
+            backgroundColor: '#222',
+            color: 'white',
+            fontWeight: 'bold',
+            border: '0px solid 0',
+          },
+          arrow: {
+            color: '#222',
+          },
+        },
+      },
+    },
+  });
+
   const actions = [
     {
-      icon: (
-        <FaCalculator size={35} className="text-light_blue dark:text-[#444]" />
-      ),
+      icon: <FaCalculator size={35} className="text-white dark:text-[#444]" />,
       name: 'TFSA Calculator',
     },
     {
-      icon: (
-        <FaCalculator size={35} className="text-light_blue dark:text-[#444]" />
-      ),
+      icon: <FaCalculator size={35} className="text-white dark:text-[#444]" />,
       name: 'FHSA Calculator',
     },
     {
-      icon: (
-        <FaCalculator size={35} className="text-light_blue dark:text-[#444]" />
-      ),
+      icon: <FaCalculator size={35} className="text-white dark:text-[#444]" />,
       name: 'FHSA Calculator',
     },
   ];
@@ -38,45 +53,52 @@ export default function Toolbox() {
         <div className="bg-white text-light_blue p-2 rounded-xl">
           <FaCalculator size={35} />
         </div> */}
-      <SpeedDial
-        ariaLabel="SpeedDial basic example"
-        sx={{ zIndex: 1 }}
-        FabProps={{
-          sx: {
-            width: 60, // change to your desired size
-            height: 60,
-            borderRadius: '12px',
-            backgroundColor: '#639db8',
-            '&:hover': {
-              backgroundColor: '#4a7f9e', // darker shade on hover
+      <ThemeProvider theme={theme}>
+        <SpeedDial
+          ariaLabel="SpeedDial basic example"
+          sx={{ zIndex: 1 }}
+          FabProps={{
+            sx: {
+              width: 60, // change to your desired size
+              height: 60,
+              borderRadius: '12px',
+              backgroundColor: '#639db8',
+              '&:hover': {
+                backgroundColor: '#4a7f9e', // darker shade on hover
+              },
             },
-          },
-        }}
-        icon={<FaWrench size={35} className=" text-white dark:text-[#444]" />}
-        direction="right"
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-            slotProps={{
-              fab: {
-                sx: {
-                  width: 60, // smaller child button size
-                  height: 60,
-                  borderRadius: '12px',
-                  backgroundColor: '#639db8',
-                  '&:hover': {
-                    backgroundColor: '#4a7f9e', // darker shade on hover
+          }}
+          icon={<FaWrench size={35} className=" text-white dark:text-[#444]" />}
+          direction="right"
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              slotProps={{
+                fab: {
+                  sx: {
+                    width: 60, // smaller child button size
+                    height: 60,
+                    borderRadius: '12px',
+                    backgroundColor: '#639db8',
+                    '&:hover': {
+                      backgroundColor: '#4a7f9e', // darker shade on hover
+                    },
                   },
                 },
-              },
-            }}
-            onClick={handleCalculatorClick}
-          />
-        ))}
-      </SpeedDial>{' '}
+                tooltip: {
+                  title: action.name,
+                  arrow: true,
+                  sx: {},
+                  placement: 'top',
+                },
+              }}
+              onClick={handleCalculatorClick}
+            />
+          ))}
+        </SpeedDial>{' '}
+      </ThemeProvider>
     </div>
   );
 }
