0eb133afe37690195ed8b39a66bfd5d1bf3bf3bd
Aiden25451 <a.asprakis@gmail.com>
2025-09-21 00:19:41 -0400

    Update callApi to work with new auth0

--- apps/frontend/src/_services/callApi.ts
+++ apps/frontend/src/_services/callApi.ts
@@ -1,36 +1,34 @@
 'use server';
+import { auth0 } from '@/lib/auth0';
 import { getAccessToken } from '@auth0/nextjs-auth0';
 import axios from 'axios';
 import { headers } from 'next/headers';
 
 export async function getHistoryApi(): Promise<string> {
-  const accessToken = await getAccessToken();
-
-  console.log('Access Token:', accessToken);
+  const { token } = await auth0.getAccessToken()
 
   const response = await axios.get(
     `${process.env.NEXT_PUBLIC_API_URL}/client/history`,
     {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
     }
   );
 
-  // return response.data.history;
   return response.data.history;
 }
 
 export async function setHistoryApi(history: string): Promise<void> {
-  const accessToken = await getAccessToken();
+  const { token } = await auth0.getAccessToken()
 
   await axios.put(
     `${process.env.NEXT_PUBLIC_API_URL}/client/update`,
     { history: history },
     {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
     }
