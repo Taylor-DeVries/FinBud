8ecec6eeb11445af13d8d1178075f319a49acb81
Aiden25451 <a.asprakis@gmail.com>
2025-11-06 22:03:35 -0500

    Adjust to using _components instead of _ui

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,4 +1,4 @@
-import QuizPage from './_ui/quiz';
+import QuizPage from './_components/quiz';
 import ResponsiveImage from '@/_components/Responsive-Image-Component/ResponsiveImage';
 import { getHistoryFunction } from '../../_lib/services/history-functions';
 
