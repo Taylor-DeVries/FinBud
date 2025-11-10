40bee299cc30f8434751dbfd3b78427a86a425bd
Aiden25451 <a.asprakis@gmail.com>
2025-11-10 17:39:47 -0500

    Simplified quiz/page.tsx

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -3,25 +3,10 @@ import ResponsiveImage from '@/_components/Responsive-Image-Component/Responsive
 import { getHistoryFunction } from '@/_lib/_services/quiz-functions';
 
 export default async function Page() {
-  try {
-    const data = await getHistoryFunction();
-    return (
-      <ResponsiveImage>
-        <QuizPage data={data}></QuizPage>
-      </ResponsiveImage>
-    );
-  } catch (error) {
-    return (
-      <ResponsiveImage>
-        <QuizPage
-          data={{
-            loading: false,
-            historyArray: [0],
-            error: 'Not logged in',
-            initialState: false,
-          }}
-        ></QuizPage>
-      </ResponsiveImage>
-    );
-  }
+  const data = await getHistoryFunction();
+  return (
+    <ResponsiveImage>
+      <QuizPage data={data}></QuizPage>
+    </ResponsiveImage>
+  );
 }
